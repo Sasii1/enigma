42d2a3c0f47a09c89ed78af05414a2de0d2af7c5
salvatoredefilippo <defilippo713@gmail.com>
2024-04-20 10:38:45 +0200

    sdfgh

--- src/scenes/End.ts
+++ src/scenes/End.ts
@@ -14,5 +14,8 @@ export default class End extends Phaser.Scene {
         const video = this.add.video(500, 300, 'End');
         video.play();
         video.setVolume(0.5);
+        this.time.delayedCall(75000, () => {
+            window.location.reload();
+        });
     }
 }
